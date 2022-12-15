4086a605cfad281c58f419dca7b51c91e01fdc31
rafay222mk <rafay222mk@gmail.com>
2022-12-15 17:20:46 +0500

    15 Dec

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,16 @@
 class UserMailer < ApplicationMailer
   def send_welcome(user)
     # debugger
+    create_reset_password_token(user)
     @user = user
     mail to: user.email, subject: 'Welcome to the site'
+  end
+  private
+  def create_reset_password_token(user)
+    raw, hashed = Devise.token_generator.generate(User, :reset_password_token)
+    @token = raw
+    user.reset_password_token = hashed
+    user.reset_password_sent_at = Time.now.utc
+    user.save
   end
 end
